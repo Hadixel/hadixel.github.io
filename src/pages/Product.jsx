545207816db2730f2b208b1e6c1c4d55c55d040a
Hadixel <hadixel@example.com>
2026-06-30 22:51:02 +0330

    done now?

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext'
 import { assets } from '../assets/assets';
 import RelatedProducts from '../components/RelatedProducts';
-
+import OurPolicy from '../components/OurPolicy';
 const Product = () => {
 
   const {productId} = useParams();
@@ -79,23 +79,7 @@ const Product = () => {
       </div>
 
       {/* policy */}
-      <div className='flex flex-col sm:flex-row justify-around gap-12 sm:gap-2 text-center py-20 text-xs sm:text-sm md:text-base text-gray-700'>
-        <div>
-            <img src={assets.exchange_icon} className='w-12 m-auto mb-5' alt="policy_icon" />
-            <p className='font-semibold'>Returns & Exchanges</p>
-            <p className='text-gray-400'>For return and warranty inquiries, please contact our support team.</p>
-        </div>
-        <div>
-            <img src={assets.quality_icon} className='w-12 m-auto mb-5' alt="policy_icon" />
-            <p className='font-semibold'>Warranty Policy</p>
-            <p className='text-gray-400'>We provide standard technical warranty for appliances</p>
-        </div>
-        <div>
-            <img src={assets.support_img} className='w-12 m-auto mb-5' alt="policy_icon" />
-            <p className='font-semibold'>Expert Support</p>
-            <p className='text-gray-400'>Contact our team for installation and service inquiries</p>
-        </div>
-      </div>
+      <OurPolicy/>
 
       {/* p features and specs */}
       <div className='mt-20'>
